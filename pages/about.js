c8766a8722c2a9e6cf3e338c06c4e6233204faf4
Mya Amos <peacockmya@gmail.com>
2022-06-02 13:27:38 -0400

    Nextjs Crash Course

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Meta from '../components/Meta'
+
+const about = () => {
+  return (
+    <div>
+      <Meta title='About' />
+      <h1>About Page</h1>
+    </div>
+  );
+};
+
+export default about;
