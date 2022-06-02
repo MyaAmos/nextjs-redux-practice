2ba39661b8aad05422c14e56d54855f8b125a5ba
Mya Amos <peacockmya@gmail.com>
2022-06-02 17:51:47 -0400

    Added Provider to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,18 @@
-import '../styles/globals.css'
-import Layout from '../components/Layout'
+import "../styles/globals.css";
+import Layout from "../components/Layout";
+import { Provider } from "react-redux";
+import { applyMiddleware, createStore } from "redux";
+
+const store = createStore(() => [], {}, applyMiddleware())
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  )
+    <Provider store={store}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/posts.js
+++ pages/posts.js
@@ -1,14 +1,15 @@
 import React from "react";
 import Posts from "../components/Posts";
 import PostsForm from "../components/PostsForm";
+import { Provider } from "react-redux";
 
 const posts = () => {
   return (
-    <>
+    <div>
       <PostsForm />
       <hr />
       <Posts />
-    </>
+    </div>
   );
 };
 
