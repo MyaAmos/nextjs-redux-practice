a35534a74a69004bb9ef0d2fed2d466b440eea8c
Mya Amos <peacockmya@gmail.com>
2022-06-02 23:58:11 -0400

    redux-added data to store and retrieved

--- components/Posts.js
+++ components/Posts.js
@@ -2,22 +2,25 @@ import { Typography } from "@mui/material";
 import React, { Component } from "react";
 import postStyles from '../styles/Posts.module.css'
 
+import PropTypes from 'prop-types'
+
+import { connect } from "react-redux";
+import { fetchPosts } from "../src/actions/postActions";
+
 export class Posts extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      posts: [],
-    };
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     posts: [],
+  //   };
+  // }
 
   componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/posts")
-      .then((res) => res.json())
-      .then((data) => this.setState({ posts: data }));
+    this.props.fetchPosts();
   }
 
   render() {
-    const postItems = this.state.posts.map((post) => (
+    const postItems = this.props.posts.map((post) => (
       <div key={post.id}>
         <Typography variant="h5" className={postStyles.title}>{post.title}</Typography>
         <Typography variant="body1">{post.body}</Typography>
@@ -34,4 +37,13 @@ export class Posts extends Component {
   }
 }
 
-export default Posts;
+Posts.propTypes = {
+  fetchPosts: PropTypes.func.isRequired,
+  posts: PropTypes.array.isRequired
+}
+
+const mapStateToProps = state => ({
+  posts: state.posts.items
+})
+
+export default connect(mapStateToProps, { fetchPosts })(Posts);
